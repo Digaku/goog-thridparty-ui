5a11d5005121d32b1eb880fa3d7593202dc06038
robin <obin.mf@gmail.com>
2011-09-04 17:41:59 +0700

    refine fullscreen loading

--- fullscreen_loading.js
+++ fullscreen_loading.js
@@ -9,22 +9,47 @@ goog.require('goog.ui.Component');
 
 
 
-goog.ui.FullscreenLoading = function(opt_domHelpers){
+goog.ui.FullscreenLoading = function(caption, auto_show, opt_domHelpers){
 	goog.ui.Component.call(this, opt_domHelpers);
+	this.auto_show_ = auto_show;
+	this.caption_ = caption || 'Loading...';
+	if(this.auto_show_ == true){
+		this.render();
+	}
 };
 goog.inherits(goog.ui.FullscreenLoading, goog.ui.Component);
 
-goog.ui.FullscreenLoading.render = function(){
-	goog.ui.FullscreenLoading.superClass_.render(goog.dom.getElement('body'));
+goog.ui.FullscreenLoading.prototype.render = function(){
 	
+	if(this.inDocument_){
+		throw Error(goog.ui.Component.Error.ALREADY_RENDERED);
+	}
+	
+	this.element_ = goog.dom.createElement('div');
 	this.bgElm_ = goog.dom.createElement('div');
+	
+	this.element_.innerHTML = '<div style="float: left;"><img src="http://www.intel.com/newsroom/assets/images/loading-spinner_32x32.gif" /></div><div style="float: left; margin-top: 10px; margin-left: 10px;">' + this.caption_ + '</div>';
+	
+	goog.dom.appendChild(document.body, this.element_);
+	goog.dom.appendChild(document.body, this.bgElm_);
 	
 	var doc = this.getDomHelper().getDocument();
 	var win = goog.dom.getWindow(doc) || window;
-	var css = {
+	var css1 = {
 		'position': 'absolute',
 		'left': '0px',
-		'top': '0px'
+		'top': '0px',
+		'background-color': '#000',
+		'z-index': '9000'
+	};
+	
+	var css2 = {
+		'padding': '10px',
+		'background-color': '#FFF',
+		'position': 'absolute',
+		'width': '210px',
+		'height': '30px',
+		'z-index': '9001'
 	};
 
 	// Take the max of scroll height and view height for cases in which document
@@ -36,10 +61,59 @@ goog.ui.FullscreenLoading.render = function(){
 	goog.style.showElement(this.bgElm_, true);
 	goog.style.setOpacity(this.bgElm_, 0.50);
 	goog.style.setSize(this.bgElm_, w, h);
-	goog.style.setStyle(this.bgElm_, css);
+	goog.style.setStyle(this.bgElm_, css1);
+	
+	goog.style.setStyle(this.element_, css2);
+	goog.style.showElement(this.element_, true);
+	
+	this.reposition();
+	
+	this.inDocument_ = true;
+	
 };
 
+goog.ui.FullscreenLoading.prototype.setVisible = function(state){
+	if(!this.inDocument_){
+		this.render();
+	}
+	
+	goog.style.showElement(this.element_, state);
+	goog.style.showElement(this.bgElm_, state);
+	
+	this.reposition();
+};
 
+goog.ui.FullscreenLoading.prototype.dispose = function(){
+	goog.ui.FullscreenLoading.superClass_.disposeInternal();
+	goog.dom.removeNode(this.element_);
+	goog.dom.removeNode(this.bgElm_);
+	
+	this.element_ = null;
+	this.bgElm_ = null;
+};
+
+goog.ui.FullscreenLoading.prototype.reposition = function(){
+	// Get the current viewport to obtain the scroll offset.
+	var doc = this.getDomHelper().getDocument();
+	var win = goog.dom.getWindow(doc) || window;
+	if (goog.style.getComputedPosition(this.getElement()) == 'fixed') {
+	  var x = 0;
+	  var y = 0;
+	} else {
+		var scroll = this.getDomHelper().getDocumentScroll();
+		var x = scroll.x;
+		var y = scroll.y;
+	}
+
+	var elmSize = goog.style.getSize(this.getElement());
+	var viewSize = goog.dom.getViewportSize(win);
+
+	// Make sure left and top are non-negatives.
+	var left = Math.max(x + viewSize.width / 2 - elmSize.width / 2, 0);
+	var top = Math.max(y + viewSize.height / 2 - elmSize.height / 2, 0);
+
+	goog.style.setPosition(this.getElement(), left, top);
+};
 
 
 
